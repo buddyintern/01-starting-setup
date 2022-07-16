37445b0a20a7e390a06988c07f214bb292826151
Firat Aksoy <aksoy.mehmet@heb.com>
2022-07-16 11:18:07 -0500

    login page aded

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -2,16 +2,15 @@ import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./ListContainer.modal.css";
+import Login from "./Login";
 // import Confetti from 'react-confetti'
 // import useWindowSize from 'react-use/lib/useWindowSize'
 
-
-function ListContainer({socket}) {
+function ListContainer({ socket }) {
   const [list, setList] = useState([]);
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [visible, setVisible] = useState(false);
   // const { width, height } = useWindowSize()
-  
-
 
   useEffect(() => {
     async function handleFetch() {
@@ -21,38 +20,34 @@ function ListContainer({socket}) {
         method: "GET",
       }).then((res) => {
         setList(res.data.data);
-        setProducts(res.data.data.products)
+        setProducts(res.data.data.products);
         // console.log(res.data.data);
       });
     }
     handleFetch();
-  }, [])
-  
-  useEffect(() => {
+  }, []);
 
-    socket.emit('connection')
+  useEffect(() => {
+    socket.emit("connection");
 
     socket.on("new product added", (data) => {
-      setProducts([...products, data])
+      setProducts([...products, data]);
       window.scrollTo(0, window.document.body.scrollHeight);
+    });
+  });
 
-      });
-
-  }) 
-
-
-
-  return (
-    <div className="container">
-      <ProductList list={products} className={list}/>
-      {/* <Confetti
-      width={width}
-      height={height}
-    /> */}
-
-    </div>
-    
-  );
+  if (visible) {
+    return (
+      <div className="container">
+        <ProductList list={products} className={list} />
+      </div>
+    );
+  }
+  else {
+    return(
+      <Login stateChanger = {setVisible}/>
+    )
+  }
 }
 
 export default ListContainer;
